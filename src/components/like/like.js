e2d328f3471a5a9f616b6aa08430769e5d5c58db
mirageN1349 <desertirpipi@gmail.com>
2020-07-25 01:09:58 +0300

    add like server

--- src/components/like/like.js
+++ src/components/like/like.js
@@ -1,33 +1,20 @@
-import { localStorageHelper } from '../../core/localStorage'
-const storage = localStorageHelper()
-
-const like = document.querySelector('.program-like'),
-  likeText = document.querySelector('.program-like span'),
-  likeImg = document.querySelector('.program-like__img')
-
-like.addEventListener('click', btnLikeClick)
-
-likeText.innerHTML = parseInt(storage.getItem('likeText')) || 120
-checkLikeState(121)
-
-storage.setItem('likeText', likeText.innerHTML)
-
-function checkLikeState(state) {
-  if (likeText.innerHTML == state) {
-    like.classList.add('active')
-    likeImg.classList.add('active')
-  } else {
-    likeImg.classList.remove('active')
-    like.classList.remove('active')
+export class Like {
+  static create(counter, softName) {
+    return fetch(`https://soft-zilla.firebaseio.com/${softName}.json`, {
+      method: 'PUT',
+      body: JSON.stringify(counter),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(response => response.json())
+      .then(Like.renderLike)
   }
-}
+  static renderLike(res) {
+    const currentLikeCounter = document.querySelector(
+      `.${res.softName} .program-like__text`
+    )
 
-function btnLikeClick() {
-  checkLikeState(120)
-  let counter = parseInt(storage.getItem('likeText'))
-  counter === 120
-    ? storage.setItem('likeText', counter + 1)
-    : storage.setItem('likeText', counter - 1)
-
-  likeText.innerHTML = parseInt(storage.getItem('likeText'))
+    currentLikeCounter.textContent = res.counter || 0
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,35 +1,85 @@
 import './scss/index.scss'
-import './components/like/like'
+import { Like } from './components/like/Like'
+import { localStorageHelper } from './core/localStorage'
 import './components/burger/burger'
 
-document.addEventListener('DOMContentLoaded', () => {
-  const btnMore = document.querySelector('.program-description__button'),
-    btnMoreImg = document.querySelector('.program-description__button img'),
-    descriptionText = document.querySelector('.program-description__text'),
-    blur = document.querySelector('.program-description__blur'),
-    headerTitle = document.querySelector('.header-title'),
-    input = document.querySelector('.header-input'),
-    hide = document.querySelectorAll('.hide')
+const storage = localStorageHelper()
 
-  btnMore.addEventListener('click', btnMoreClick)
+document.addEventListener('DOMContentLoaded', () => {
+  const input = document.querySelector('.header-input'),
+    hide = document.querySelectorAll('.hide'),
+    likeArr = document.querySelectorAll('.program-like')
+
+  likeArr.forEach(like => {
+    likeClick(like, false)()
+    like.addEventListener('click', likeClick(like, true))
+  })
 
   if (document.documentElement.clientWidth <= 768) {
-    headerTitle.textContent = 'Skype'
-
     hide.forEach(el => (el.style.display = 'none'))
 
     input.addEventListener('click', inputClick)
 
     function inputClick() {
       input.value = ''
-
       input.classList.toggle('active')
     }
   }
 
-  function btnMoreClick() {
-    blur.classList.toggle('active')
-    descriptionText.classList.toggle('active')
-    btnMoreImg.classList.toggle('active')
+  function likeClick(like, check) {
+    const programs = ['skype', 'whatsapp', 'zoom']
+    return function () {
+      programs.forEach(program => {
+        const currentLike = like.closest(`.${program}`)
+        if (currentLike) {
+          const softName = currentLike.classList[1]
+          getRequest(softName, check, program, like)
+        }
+      })
+    }
+  }
+
+  function getRequest(softName, check, program, currentLike) {
+    fetch(`https://soft-zilla.firebaseio.com/${softName}.json`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(response => response.json())
+      .then(res => {
+        const prevLikeState = storage.getItem(program) || false
+        prevLikeState
+          ? currentLike.classList.add('active')
+          : currentLike.classList.remove('active')
+
+        let counter = res.counter || 0
+
+        if (check) {
+          storage.setItem(program, prevLikeState)
+
+          if (prevLikeState === false) {
+            currentLike.classList.add('active')
+            counter = res.counter + 1
+            storage.setItem(program, true)
+          } else {
+            currentLike.classList.remove('active')
+            counter = res.counter - 1
+            storage.setItem(program, false)
+          }
+        }
+
+        const text = {
+          counter,
+          softName,
+        }
+        putRequest(text, softName)
+      })
+  }
+
+  function putRequest(text, softName) {
+    Like.create(text, softName).then(() => {
+      console.log('finish')
+    })
   }
 })
